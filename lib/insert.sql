5ff9db7d751bbea0f14336826b1730625ce6e431
gabystern <stern.gabrielle@gmail.com>
2017-04-10 16:40:51 -0400

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+-- INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+
+-- CREATE TABLE bears (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER,
+--   gender CHAR(1)
+--   color TEXT,
+--   temperament TEXT,
+--   alive NUMERIC
+-- );
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "green", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 11, "F", "purple", "happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5, "M", "yellow", "sad", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 50, "M", "brown", "angry", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 25, "F", "pink", "chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "M", "green", "grumpy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 17, "F", "white", "anxious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "M", "green", "happy", 1);
